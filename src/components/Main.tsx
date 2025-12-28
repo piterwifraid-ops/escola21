7e622b53a92c56fdb63af2032c1a225488c47336
piterwifraid-ops <piterwifraid@gmail.com>
2025-12-28 16:38:53 -0200

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -77,7 +77,7 @@ const Main: React.FC = memo(() => {
 			{/* Banner */}
 			<div className="px-4 pb-6">
 				<img 
-					src="../dist/assets/IMAGEM.webp" 
+					src=".../dist/assets/IMAGEM.webp" 
 					alt="Concurso Público Nacional Unificado" 
 					loading="eager"
 					className="max-w-full h-auto"
